3a13ca0a6df691572f98c8584b95ba8850792c99
Thk <th.perche@gmail.com>
2020-10-22 00:02:27 +0200

    initial commit

---
+++ src/db/db.js
@@ -0,0 +1,11 @@
+/**====================================*\
+ *  MONGOOSE CONFIGURATION
+ ======================================*/
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
